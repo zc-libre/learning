ff31be33e480c518bd38b10a0226223fd79b743b
zc-libre <zc150622@gmail.com>
2021-02-08 02:25:22 +0800

    更新gitignore

---
+++ netty-demo/src/main/java/com/libre/netty/bio/BioServer.java
@@ -0,0 +1,56 @@
+package com.libre.netty.bio;
+
+import com.libre.netty.util.ThreadUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.*;
+
+/**
+ * @author zhao.cheng
+ * @Date 2021/2/8
+ */
+
+public class BioServer {
+
+    public static void main(String[] args) throws IOException {
+        ThreadPoolExecutor executor = ThreadUtils.executor();
+
+        ServerSocket serverSocket = new ServerSocket(8081);
+        System.out.println("服务器启动了");
+
+       while (true) {
+           System.out.println("线程信息： id: " + Thread.currentThread().getId() + "," + "线程信息： name: " + Thread.currentThread().getName());
+
+           System.out.println("等待连接......");
+           final Socket socket = serverSocket.accept();
+           System.out.println("连接到一个客户端");
+
+           executor.execute(() -> handle(socket));
+       }
+    }
+
+    public static void handle(Socket socket) {
+
+        try (InputStream in = socket.getInputStream()){
+
+            System.out.println("线程信息： id: " + Thread.currentThread().getId() + "," + "线程信息： name: " + Thread.currentThread().getName());
+
+            byte[] buffer = new byte[1024];
+
+            System.out.println("等待读取数据......");
+            while (in.read(buffer) != -1) {
+                System.out.println("线程信息： id: " + Thread.currentThread().getId() + "," + "线程信息： name: " + Thread.currentThread().getName());
+
+                String line = new String(buffer, 0, buffer.length);
+                System.out.println("接收到消息：[" + line + "]" );
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ netty-demo/src/main/java/com/libre/netty/util/ThreadUtils.java
@@ -0,0 +1,24 @@
+package com.libre.netty.util;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zhao.cheng
+ * @Date 2021/2/8
+ */
+public class ThreadUtils {
+
+   private static final ThreadPoolExecutor EXECUTOR = new ThreadPoolExecutor(2,
+            5,
+            10, TimeUnit.MINUTES,
+            new LinkedBlockingQueue<>(),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.CallerRunsPolicy());
+
+   public static ThreadPoolExecutor executor() {
+       return EXECUTOR;
+   }
+}
