e8bb2d26ed833fcce4230e428a579b36fedc9d4f
zc150622 <504879189@qq.com>
2021-07-19 23:10:26 +0800

    feat: redisson-demo

---
+++ Redisson-demo/src/main/java/com/libre/redisson/RedissonApplication.java
@@ -0,0 +1,15 @@
+package com.libre.redisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZC
+ * @date 2021/7/19 15:37
+ */
+@SpringBootApplication
+public class RedissonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonApplication.class, args);
+    }
+}
